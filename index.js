de4f3132b910149fa65057193d04d9a02c23f41a
sathishd_recode <sathishkumar.durairaj@kamerai.ai>
2024-10-23 15:26:04 +0530

    api changes

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Hello World' });
 });
-
+app.get('/about', (req, res) => {
+    res.status(200).json({ message: 'About Us', data : [] });
+});
+app.get('/contact/:id', (req, res) => {
+    res.status(200).json({ message: 'Contact Us', data : req.params.id });
+})
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
